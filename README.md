3c24ef5ab577231518e0f328e0bd377252d90bf1
egeacr <egeacr@users.noreply.github.com>
2024-09-17 21:04:20 +0000

    deploy: 770a0cebca841fc88fc375e0b72e06dcb3f3db32

--- README.md
+++
@@ -1,2 +0,0 @@
-# SeleniumRestAssuredCucumberAllure
-Java automation project with Selenium for UI and Rest Assured for API tests. 
--- src/test/java/demo/pages/Utils.java
+++
@@ -1,9 +0,0 @@
-package demo.pages;
-
-import org.openqa.selenium.WebDriver;
-
-public class Utils extends BasePage{
-    public Utils(WebDriver driver) {
-        super(driver);
-    }
-}
